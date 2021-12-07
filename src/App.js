a3cd484443505af84fe1c3dc26d0e0d41d2b915a
Anthony P <43166263+anthonypz@users.noreply.github.com>
2021-12-06 23:45:25 -0800

    Simplify game description

--- src/App.js
+++ src/App.js
@@ -116,8 +116,8 @@ export default function App() {
       {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
-        Roll until all dice are the same. Click each die to freeze it at its
-        current value between rolls.
+        Roll until all dice are the same. Click each dice to freeze it at its
+        current value.
       </p>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
